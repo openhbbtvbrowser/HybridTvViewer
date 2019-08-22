9b18a09050d1cb6374c72315d7f788649eeddfa7
Volker Jung <jung@smartclip.de>
2019-08-22 12:16:20 +0200

    FIX: polyfill standalone initialization

--- src/js/hbbtv-polyfill/index.js
+++ src/js/hbbtv-polyfill/index.js
@@ -2,16 +2,23 @@ import { keyEventInit } from "./keyevent-init.js";
 import { hbbtvFn } from "./hbbtv.js";
 import { VideoHandler } from "./hbb-video-handler.js";
 
-console.log("hbbtv-polyfill: load");
+function init() {
+    console.log("hbbtv-polyfill: load");
 
-// set body position
-document.body.style.position = "absolute";
-document.body.style.width = "1280px";
-document.body.style.height = "720px";
+    // set body position
+    document.body.style.position = "absolute";
+    document.body.style.width = "1280px";
+    document.body.style.height = "720px";
 
-keyEventInit();
-hbbtvFn();
+    keyEventInit();
+    hbbtvFn();
 
-new VideoHandler().initialize();
+    new VideoHandler().initialize();
 
-console.log("hbbtv-polyfill: loaded");
+    console.log("hbbtv-polyfill: loaded");
+}
+if (!document.body) {
+    document.addEventListener("DOMContentLoaded", init);
+} else {
+    init();
+}
